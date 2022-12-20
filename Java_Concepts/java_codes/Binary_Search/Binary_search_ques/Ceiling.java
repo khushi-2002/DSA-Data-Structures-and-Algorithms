3e05363efe27758cf10e62bd8701925ac5e52459
khushi-2002 <khushiagarwal915@gmail.com>
2022-12-20 21:36:01 +0530

    Added- Day1

--- Java_Concepts/java_codes/Binary_Search/Binary_search_ques/Ceiling.java
+++ Java_Concepts/java_codes/Binary_Search/Binary_search_ques/Ceiling.java
@@ -4,15 +4,20 @@ public class Ceiling {
     public static void main(String[] args) {
         int []arr = {10,12,14,16,17,19};
         System.out.println(ceil(arr, 18));
+        System.out.println(floor(arr, 11));
     }
-    
+
 
     static int ceil(int []arr, int target){
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
+
         int s = 0;
         int e = arr.length-1;
         int mid = s+(e-s)/2;
 
-        while(s<e){
+        while(s<=e){
             if(target==arr[mid]){
                 return mid;
             }
@@ -24,7 +29,32 @@ public class Ceiling {
             mid = s+(e-s)/2;
         }
 
-        return arr[e+1];
+        return arr[s];
+
+    }
+
+    static int floor(int []arr, int target){
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
+        
+        int s = 0;
+        int e = arr.length-1;
+        int mid = s+(e-s)/2;
+
+        while(s<=e){
+            if(target==arr[mid]){
+                return mid;
+            }
+            else if(target>arr[mid]){
+                s=mid+1;            }
+            else{
+                e=mid-1;
+            }
+            mid = s+(e-s)/2;
+        }
+
+        return arr[e];
 
     }
 
---
+++ Java_Concepts/java_codes/DailyCode/Day1.java
@@ -0,0 +1,72 @@
+package java_codes.DailyCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Question: Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+*/
+public class Day1 {
+    public static void main(String[] args) {
+       
+       int []arr = {
+        12, 23, 45, 67
+       };
+       int target = 68;
+       System.out.println(search_pairs(arr, target));
+       System.out.println(search_pairs2(arr, target));
+       }
+// Approach 1 - Using For loop 
+       static boolean search_pairs(int []arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <arr.length ; j++) {
+                if(arr[i]+arr[j]==target){
+                    return true;
+                }
+            }
+       }
+        return false;
+      
+    }
+
+// Approach 2 -- Using Map
+    static boolean search_pairs2(int []arr, int target){
+        if(arr.length==0 || arr==null){
+            return false;
+        }
+        ArrayList<Integer> m = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            if(m.contains(target-arr[i])){
+                return true;
+            }
+
+            m.add(arr[i]);
+
+        }
+        return false;
+    }
+// Approach 3 - Using ArrayList
+    static boolean search_pairs3(int []arr, int target){
+        if(arr.length==0 || arr==null){
+            return false;
+        }
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            if(m.containsKey(target-arr[i])){
+                return true;
+            }
+
+            m.put(arr[i], i);
+
+        }
+        return false;
+    }
+}
+
